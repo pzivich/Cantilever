dbb407ee8ae8d97d4b622b210144846dccbd8cb7
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2025-04-09 10:26:43 -0400

    Separate utils from efuncs

--- cantilever/estimators/time_to_event/efuncs.py
+++ cantilever/estimators/time_to_event/efuncs.py
@@ -2,7 +2,7 @@ import numpy as np
 from delicatessen.estimating_equations import ee_regression
 from delicatessen.utilities import inverse_logit
 
-from cantilever.estimators.time_to_event.utils import align_ipcw_with_event_times
+from cantilever.estimators.time_to_event.utils import construct_weights, plogit_predictions
 
 
 def ef_sample_logit(theta, s, sample_matrix):
@@ -52,135 +52,6 @@ def ef_weight_models(theta, s, a, c, sample_matrix, action_matrix, censor_matrix
         return np.vstack([ee_sample, ee_action, ee_censor1, ee_censor0])
 
 
-def construct_weights(theta, s, a, sample_matrix, action_matrix, censor_matrix=None,
-                      time_matrix_s1=None, strata_s1_times=None, time_matrix_s0=None, strata_s0_times=None,
-                      unique_censor_times=None, unique_event_times=None):
-    if censor_matrix is None:
-        m_index1 = sample_matrix.shape[1]
-        beta = theta[:m_index1]
-        gamma = theta[m_index1:]
-
-        # Constructing the inverse odds of sampling weights
-        pr_s = inverse_logit(np.dot(sample_matrix, beta))
-        iosw = s + ((1-s)*(1-pr_s)/pr_s)
-
-        # Constructing the inverse probability of treatment weights
-        gamma1 = gamma[:action_matrix.shape[1]]
-        gamma0 = gamma[action_matrix.shape[1]:]
-        pr_a2 = inverse_logit(np.dot(action_matrix, gamma1))
-        pr_a1 = inverse_logit(np.dot(action_matrix, gamma0))
-        iptw = ((a == 2) / pr_a2
-                + (a == 1)*(s == 1) / (1-pr_a2)
-                + (a == 1)*(s == 0) / pr_a1
-                + (a == 0) / (1-pr_a1))
-        ipw = iosw * iptw
-        return ipw
-    else:
-        m_index1 = sample_matrix.shape[1]
-        m_index2 = m_index1 + action_matrix.shape[1]*2
-        m_index3 = censor_matrix.shape[1] + time_matrix_s1.shape[0]
-        beta = theta[:m_index1]
-        gamma = theta[m_index1:m_index2]
-        phi = theta[m_index2:]
-        phi1 = phi[:m_index3]
-        phi0 = phi[m_index3:]
-
-        # Constructing the inverse odds of sampling weights
-        pr_s = inverse_logit(np.dot(sample_matrix, beta))
-        iosw = s + ((1 - s) * (1 - pr_s) / pr_s)
-
-        # Constructing the inverse probability of treatment weights
-        gamma1 = gamma[:action_matrix.shape[1]]
-        gamma0 = gamma[action_matrix.shape[1]:]
-        pr_a2 = inverse_logit(np.dot(action_matrix, gamma1))
-        pr_a1 = inverse_logit(np.dot(action_matrix, gamma0))
-        iptw = ((a == 2) / pr_a2
-                + (a == 1) * (s == 1) / (1 - pr_a2)
-                + (a == 1) * (s == 0) / pr_a1
-                + (a == 0) / (1 - pr_a1))
-
-        # Constructing the inverse probability of censoring weights
-        pr_c1 = 1 - plogit_predictions(theta=phi1, baseline_matrix=censor_matrix, time_matrix=time_matrix_s1,
-                                       strata_unique_times=strata_s1_times, all_unique_times=unique_censor_times)
-        pr_c0 = 1 - plogit_predictions(theta=phi0, baseline_matrix=censor_matrix, time_matrix=time_matrix_s0,
-                                       strata_unique_times=strata_s0_times, all_unique_times=unique_censor_times)
-        ipcw = 1 / (pr_c1 * s + pr_c0 * (1 - s))
-
-        # Aligning censor weights
-        ipcw = align_ipcw_with_event_times(event_time_vector=unique_event_times, censor_time_vector=unique_censor_times,
-                                           censor_weights=ipcw)
-
-        # Computing the overall weight
-        ipw = (iosw * iptw)[:, None] * ipcw
-        return ipw.T
-
-
-def ef_weight_models_censor(theta, s, a, c, sample_matrix, action_matrix, censor_matrix,
-                            time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, contribute_s1, strata_s1_times,
-                            time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, contribute_s0, strata_s0_times):
-    m_index1 = sample_matrix.shape[1]
-    m_index2 = m_index1 + action_matrix.shape[1]
-    m_index3 = censor_matrix.shape[1] + len(strata_s1_times)
-    beta = theta[:m_index1]
-    gamma = theta[m_index1:m_index2]
-    phi = theta[m_index2:]
-    phi1 = phi[:m_index3]
-    phi0 = phi[m_index3:]
-
-    # Nuisance models
-    ee_sample = ef_sample_logit(beta, s, sample_matrix)
-    ee_action = ef_action_logit(gamma, s, a, action_matrix)
-    ee_censor1 = ef_pooled_logit(theta=phi1, delta=c, baseline_matrix=censor_matrix,
-                                 time_matrix=time_matrix_s1, final_time_matrix=final_time_matrix_s1,
-                                 risk_set_matrix=risk_set_matrix_s1, contribute=contribute_s1)
-    ee_censor0 = ef_pooled_logit(theta=phi0, delta=c, baseline_matrix=censor_matrix,
-                                 time_matrix=time_matrix_s0, final_time_matrix=final_time_matrix_s0,
-                                 risk_set_matrix=risk_set_matrix_s0, contribute=contribute_s0)
-    return np.vstack([ee_sample, ee_action, ee_censor1, ee_censor0])
-
-
-def construct_weights_censor(theta, s, a, sample_matrix, action_matrix, censor_matrix,
-                             time_matrix_s1, strata_s1_times, time_matrix_s0, strata_s0_times,
-                             unique_censor_times, unique_event_times):
-    m_index1 = sample_matrix.shape[1]
-    m_index2 = m_index1 + action_matrix.shape[1]
-    m_index3 = censor_matrix.shape[1] + len(strata_s1_times)
-    beta = theta[:m_index1]
-    gamma = theta[m_index1:m_index2]
-    phi = theta[m_index2:]
-    phi1 = phi[:m_index3]
-    phi0 = phi[m_index3:]
-
-    # Constructing the inverse odds of sampling weights
-    pr_s = inverse_logit(np.dot(sample_matrix, beta))
-    iosw = s + ((1-s)*(1-pr_s)/pr_s)
-
-    # Constructing the inverse probability of treatment weights
-    gamma1 = gamma[:action_matrix.shape[1]]
-    gamma0 = gamma[action_matrix.shape[1]:]
-    pr_a2 = inverse_logit(np.dot(action_matrix, gamma1))
-    pr_a1 = inverse_logit(np.dot(action_matrix, gamma0))
-    iptw = ((a == 2) / pr_a2
-            + (a == 1)*(s == 1) / (1-pr_a2)
-            + (a == 1)*(s == 0) / pr_a1
-            + (a == 0) / (1-pr_a1))
-
-    # Constructing the inverse probability of censoring weights
-    ipcw_s1 = 1 - plogit_predictions(theta=phi1, baseline_matrix=censor_matrix, time_matrix=time_matrix_s1,
-                                     strata_unique_times=strata_s1_times, all_unique_times=unique_censor_times)
-    ipcw_s0 = 1 - plogit_predictions(theta=phi0, baseline_matrix=censor_matrix, time_matrix=time_matrix_s0,
-                                     strata_unique_times=strata_s0_times, all_unique_times=unique_censor_times)
-    ipcw = ipcw_s1*s + ipcw_s0*(1-s)
-
-    # Aligning censor weights
-    ipcw = align_ipcw_with_event_times(event_time_vector=unique_event_times, censor_time_vector=unique_censor_times,
-                                       censor_weights=ipcw)
-
-    # Computing the overall weight
-    ipw = iosw * iptw * ipcw
-    return ipw
-
-
 def ef_product_limit(theta, delta, final_time_matrix, risk_set_matrix, contributions, weights=None):
     delta = np.asarray(delta)                            # Convert delta to NumPy array
     theta = np.asarray(theta)                            # Convert theta to NumPy array
@@ -200,22 +71,10 @@ def ef_product_limit(theta, delta, final_time_matrix, risk_set_matrix, contribut
     return ee_risk * contributions
 
 
-def product_limit_predict(delta, final_time_matrix, risk_set_matrix, contributions, weights=None):
-    if weights is None:
-        weights = 1
-
-    numer_matrix = final_time_matrix * delta * weights * contributions
-    denom_matrix = risk_set_matrix * weights * contributions
-    numerator = np.sum(numer_matrix, axis=1)
-    denominator = np.sum(denom_matrix, axis=1)
-    return np.cumprod(1 - (numerator / denominator))
-
-
-def ef_risk_product_limit(theta, s, a, c, delta, sample_matrix, action_matrix, censor_matrix,
-                          time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, strata_s1_times,
-                          time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, strata_s0_times,
-                          final_time_matrix, risk_set_matrix, contribute,
-                          unique_censor_times, unique_event_times):
+def ef_risk_ipw(theta, s, a, c, delta, sample_matrix, action_matrix, censor_matrix,
+                time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, strata_s1_times,
+                time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, strata_s0_times,
+                final_time_matrix, risk_set_matrix, contribute, unique_censor_times, unique_event_times):
     n_unique_times = risk_set_matrix.shape[0]
     alpha = theta[:n_unique_times]
     beta_all = theta[n_unique_times:]
@@ -250,11 +109,11 @@ def ef_risk_product_limit(theta, s, a, c, delta, sample_matrix, action_matrix, c
     return np.vstack([ee_risk, ee_weights])
 
 
-def ef_diagnostic_product_limit(theta, s, a, c,
-                                delta, sample_matrix, action_matrix, censor_matrix,
-                                time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, strata_s1_times,
-                                time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, strata_s0_times,
-                                final_time_matrix, risk_set_matrix, unique_event_times, unique_censor_times):
+def ef_diagnostic_ipw(theta, s, a, c,
+                      delta, sample_matrix, action_matrix, censor_matrix,
+                      time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, strata_s1_times,
+                      time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, strata_s0_times,
+                      final_time_matrix, risk_set_matrix, unique_event_times, unique_censor_times):
     n_unique_times = risk_set_matrix.shape[0]
     obs_n = delta.shape[0]
     ird = theta[0]
@@ -332,46 +191,8 @@ def ef_pooled_logit(theta, delta, baseline_matrix, time_matrix, final_time_matri
     return score_plogit * contribute
 
 
-def plogit_predictions(theta, baseline_matrix, time_matrix, strata_unique_times, all_unique_times):
-    baseline_n_params = baseline_matrix.shape[1]
-    obs_n = baseline_matrix.shape[0]
-    beta_x = theta[:baseline_n_params]
-    beta_s = np.asarray(theta[baseline_n_params:])
-    n_time_steps = time_matrix.shape[1]
-
-    # Log-odds contributions for covariate and time
-    log_odds_w = np.dot(baseline_matrix, beta_x)
-    log_odds_t = np.dot(time_matrix, beta_s)
-
-    # # Computing full matrix of predicted values for each time
-    log_odds_w_matrix = np.tile(log_odds_w, (n_time_steps, 1))       # Stacked copies of X contributions for intervals
-    y_pred = inverse_logit(log_odds_w_matrix + log_odds_t[:, None])  # Predicted event at time intervals matrix
-    survival_prediction = np.cumprod(1 - y_pred, axis=0)
-    risk_matrix = 1 - survival_prediction
-    risk_t0 = 0
-
-    # Generating predictions
-    predictions = []
-    for aut in all_unique_times:
-        if aut == 0 or aut < strata_unique_times[0]:
-            prediction = np.ones(obs_n) * risk_t0
-        else:
-            if strata_unique_times[-1] <= aut:
-                matrix_index = -1
-            else:
-                further_times = strata_unique_times[aut < strata_unique_times]
-                if len(further_times) < 1:
-                    nearest = strata_unique_times[aut >= strata_unique_times][-1]
-                else:
-                    nearest = strata_unique_times[aut < strata_unique_times][0]
-                matrix_index = np.where(strata_unique_times == nearest)[0][0] - 1
-            prediction = risk_matrix[matrix_index, :]
-        predictions.append(prediction)
-    return np.asarray(predictions)
-
-
-def ef_risk_function(theta, delta, baseline_matrix, time_matrix, final_time_matrix, risk_set_matrix, contribute,
-                     strata_unique_times, all_unique_times):
+def ef_risk_gcomp(theta, delta, baseline_matrix, time_matrix, final_time_matrix, risk_set_matrix, contribute,
+                  strata_unique_times, all_unique_times):
     n_unique_times = len(all_unique_times)
     alpha = theta[:n_unique_times]
     beta = theta[n_unique_times:]
@@ -394,10 +215,10 @@ def ef_risk_function(theta, delta, baseline_matrix, time_matrix, final_time_matr
     return np.vstack([ee_risks, ee_plogit])
 
 
-def ef_diagnostic_function(theta, delta, baseline_matrix, sample,
-                           time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, contribute_s1, strata_s1_times,
-                           time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, contribute_s0, strata_s0_times,
-                           all_unique_times):
+def ef_diagnostic_gcomp(theta, delta, baseline_matrix, sample,
+                        time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, contribute_s1, strata_s1_times,
+                        time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, contribute_s0, strata_s0_times,
+                        all_unique_times):
     n_unique_times = len(all_unique_times)
     obs_n = delta.shape[0]
     plogit_index = 1 + n_unique_times + baseline_matrix.shape[1] + len(strata_s1_times)
@@ -440,10 +261,10 @@ def ef_diagnostic_function(theta, delta, baseline_matrix, sample,
     return np.vstack([ee_ird, ee_diag, ee_plogit1, ee_plogit0])
 
 
-def ef_single_span_function(theta, delta, baseline_matrix, sample,
-                            time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, contribute_s1, strata_s1_times,
-                            time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, contribute_s0, strata_s0_times,
-                            all_unique_times):
+def ef_single_span_gcomp(theta, delta, baseline_matrix, sample,
+                         time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, contribute_s1, strata_s1_times,
+                         time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, contribute_s0, strata_s0_times,
+                         all_unique_times):
     n_unique_times = len(all_unique_times)
     plogit_index = n_unique_times + baseline_matrix.shape[1] + len(strata_s1_times)
     alpha = theta[:n_unique_times]
@@ -480,12 +301,12 @@ def ef_single_span_function(theta, delta, baseline_matrix, sample,
     return np.vstack([ee_rd, ee_plogit1, ee_plogit0])
 
 
-def ef_multi_span_function(theta, delta, baseline_matrix, sample,
-                           time_matrix_s3, final_time_matrix_s3, risk_set_matrix_s3, contribute_s3, strata_s3_times,
-                           time_matrix_s2, final_time_matrix_s2, risk_set_matrix_s2, contribute_s2, strata_s2_times,
-                           time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, contribute_s1, strata_s1_times,
-                           time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, contribute_s0, strata_s0_times,
-                           all_unique_times):
+def ef_multi_span_gcomp(theta, delta, baseline_matrix, sample,
+                        time_matrix_s3, final_time_matrix_s3, risk_set_matrix_s3, contribute_s3, strata_s3_times,
+                        time_matrix_s2, final_time_matrix_s2, risk_set_matrix_s2, contribute_s2, strata_s2_times,
+                        time_matrix_s1, final_time_matrix_s1, risk_set_matrix_s1, contribute_s1, strata_s1_times,
+                        time_matrix_s0, final_time_matrix_s0, risk_set_matrix_s0, contribute_s0, strata_s0_times,
+                        all_unique_times):
     n_unique_times = len(all_unique_times)
     plogit_index3 = n_unique_times + baseline_matrix.shape[1] + len(strata_s3_times)
     plogit_index2 = plogit_index3 + baseline_matrix.shape[1] + len(strata_s2_times)
--- cantilever/estimators/time_to_event/utils.py
+++ cantilever/estimators/time_to_event/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from delicatessen.utilities import inverse_logit
 
 
 def align_ipcw_with_event_times(event_time_vector, censor_time_vector, censor_weights):
@@ -20,3 +21,117 @@ def align_ipcw_with_event_times(event_time_vector, censor_time_vector, censor_we
             else:
                 ipcw[:, i] = censor_weights[:, j-1]
     return ipcw
+
+
+def product_limit_predict(delta, final_time_matrix, risk_set_matrix, contributions, weights=None):
+    if weights is None:
+        weights = 1
+
+    numer_matrix = final_time_matrix * delta * weights * contributions
+    denom_matrix = risk_set_matrix * weights * contributions
+    numerator = np.sum(numer_matrix, axis=1)
+    denominator = np.sum(denom_matrix, axis=1)
+    return np.cumprod(1 - (numerator / denominator))
+
+
+def construct_weights(theta, s, a, sample_matrix, action_matrix, censor_matrix=None,
+                      time_matrix_s1=None, strata_s1_times=None, time_matrix_s0=None, strata_s0_times=None,
+                      unique_censor_times=None, unique_event_times=None):
+    if censor_matrix is None:
+        m_index1 = sample_matrix.shape[1]
+        beta = theta[:m_index1]
+        gamma = theta[m_index1:]
+
+        # Constructing the inverse odds of sampling weights
+        pr_s = inverse_logit(np.dot(sample_matrix, beta))
+        iosw = s + ((1-s)*(1-pr_s)/pr_s)
+
+        # Constructing the inverse probability of treatment weights
+        gamma1 = gamma[:action_matrix.shape[1]]
+        gamma0 = gamma[action_matrix.shape[1]:]
+        pr_a2 = inverse_logit(np.dot(action_matrix, gamma1))
+        pr_a1 = inverse_logit(np.dot(action_matrix, gamma0))
+        iptw = ((a == 2) / pr_a2
+                + (a == 1)*(s == 1) / (1-pr_a2)
+                + (a == 1)*(s == 0) / pr_a1
+                + (a == 0) / (1-pr_a1))
+        ipw = iosw * iptw
+        return ipw
+    else:
+        m_index1 = sample_matrix.shape[1]
+        m_index2 = m_index1 + action_matrix.shape[1]*2
+        m_index3 = censor_matrix.shape[1] + time_matrix_s1.shape[0]
+        beta = theta[:m_index1]
+        gamma = theta[m_index1:m_index2]
+        phi = theta[m_index2:]
+        phi1 = phi[:m_index3]
+        phi0 = phi[m_index3:]
+
+        # Constructing the inverse odds of sampling weights
+        pr_s = inverse_logit(np.dot(sample_matrix, beta))
+        iosw = s + ((1 - s) * (1 - pr_s) / pr_s)
+
+        # Constructing the inverse probability of treatment weights
+        gamma1 = gamma[:action_matrix.shape[1]]
+        gamma0 = gamma[action_matrix.shape[1]:]
+        pr_a2 = inverse_logit(np.dot(action_matrix, gamma1))
+        pr_a1 = inverse_logit(np.dot(action_matrix, gamma0))
+        iptw = ((a == 2) / pr_a2
+                + (a == 1) * (s == 1) / (1 - pr_a2)
+                + (a == 1) * (s == 0) / pr_a1
+                + (a == 0) / (1 - pr_a1))
+
+        # Constructing the inverse probability of censoring weights
+        pr_c1 = 1 - plogit_predictions(theta=phi1, baseline_matrix=censor_matrix, time_matrix=time_matrix_s1,
+                                       strata_unique_times=strata_s1_times, all_unique_times=unique_censor_times)
+        pr_c0 = 1 - plogit_predictions(theta=phi0, baseline_matrix=censor_matrix, time_matrix=time_matrix_s0,
+                                       strata_unique_times=strata_s0_times, all_unique_times=unique_censor_times)
+        ipcw = 1 / (pr_c1 * s + pr_c0 * (1 - s))
+
+        # Aligning censor weights
+        ipcw = align_ipcw_with_event_times(event_time_vector=unique_event_times, censor_time_vector=unique_censor_times,
+                                           censor_weights=ipcw)
+
+        # Computing the overall weight
+        ipw = (iosw * iptw)[:, None] * ipcw
+        return ipw.T
+
+
+def plogit_predictions(theta, baseline_matrix, time_matrix, strata_unique_times, all_unique_times):
+    baseline_n_params = baseline_matrix.shape[1]
+    obs_n = baseline_matrix.shape[0]
+    beta_x = theta[:baseline_n_params]
+    beta_s = np.asarray(theta[baseline_n_params:])
+    n_time_steps = time_matrix.shape[1]
+
+    # Log-odds contributions for covariate and time
+    log_odds_w = np.dot(baseline_matrix, beta_x)
+    log_odds_t = np.dot(time_matrix, beta_s)
+
+    # # Computing full matrix of predicted values for each time
+    log_odds_w_matrix = np.tile(log_odds_w, (n_time_steps, 1))       # Stacked copies of X contributions for intervals
+    y_pred = inverse_logit(log_odds_w_matrix + log_odds_t[:, None])  # Predicted event at time intervals matrix
+    survival_prediction = np.cumprod(1 - y_pred, axis=0)
+    risk_matrix = 1 - survival_prediction
+    risk_t0 = 0
+
+    # Generating predictions
+    predictions = []
+    for aut in all_unique_times:
+        if aut == 0 or aut < strata_unique_times[0]:
+            prediction = np.ones(obs_n) * risk_t0
+        else:
+            if strata_unique_times[-1] <= aut:
+                matrix_index = -1
+            else:
+                further_times = strata_unique_times[aut < strata_unique_times]
+                if len(further_times) < 1:
+                    nearest = strata_unique_times[aut >= strata_unique_times][-1]
+                else:
+                    nearest = strata_unique_times[aut < strata_unique_times][0]
+                matrix_index = np.where(strata_unique_times == nearest)[0][0] - 1
+            prediction = risk_matrix[matrix_index, :]
+        predictions.append(prediction)
+    return np.asarray(predictions)
+
+
